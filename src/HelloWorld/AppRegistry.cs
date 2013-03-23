bf53220d0b5357274bf5b2a327ae1a2d1ff922b9
jfebres <jaime.febres@gmail.com>
2013-03-22 19:03:18 -0500

    Setup of HelloWorld project.

---
+++ src/HelloWorld/AppRegistry.cs
@@ -0,0 +1,9 @@
+﻿using FubuMVC.Core;
+
+namespace HelloWorld
+{
+    public class AppRegistry : FubuRegistry
+    {
+
+    }
+}
---
+++ src/HelloWorld/Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FubuMVC.Core;
+using FubuMVC.StructureMap;
+
+namespace HelloWorld
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            FubuApplication
+                .For<AppRegistry>()
+                .StructureMapObjectFactory()
+                .Bootstrap();
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
